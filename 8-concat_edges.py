1e9a45e22d0e1b278e5a89d911b5512f31e6cf9f
aniebietafia <aniebietafia87@gmail.com>
2023-08-11 09:20:39 +0100

    refactoring

---
+++ 8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(str[39:54] + " " + str[55:66] + " " + str[107:111] + " " + str[0:6])
