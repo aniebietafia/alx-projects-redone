bd1c417a04f47dd461abfdd8915629942df93d6e
aniebietafia <aniebietafia87@gmail.com>
2023-08-23 19:19:07 +0100

    promise with .then

--- alx-backend-specialization/javascript-projects/ES6_promise/2-then.js
+++ alx-backend-specialization/javascript-projects/ES6_promise/2-then.js
@@ -11,6 +11,7 @@ export default function handleResponseFromAPI(promise) {
       console.log("Got a response from the API", resp);
     })
     .catch((err) => {
+      console.log(err);
       throw new Error();
     });
   return result;
