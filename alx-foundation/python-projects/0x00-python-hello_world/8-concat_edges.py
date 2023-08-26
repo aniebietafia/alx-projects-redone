0941e5f0535088086b6dc640cea99d747950d321
aniebietafia <aniebietafia87@gmail.com>
2023-08-26 16:19:56 +0100

    concat edges

---
+++ alx-foundation/python-projects/0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
