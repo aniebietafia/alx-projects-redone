ae56897d2dec5a80390b74992e97435cd187cde0
aniebietafia <aniebietafia87@gmail.com>
2023-08-26 15:44:43 +0100

    run python file

--- alx-foundation/python-projects/0x00-python-hello_world/3-print_number.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-number = 98
-print(f"{number} Battery Street")
-print("{} Battery Street".format(number))
--- alx-foundation/python-projects/0x00-python-hello_world/4-print_float.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-number = 3.14159
-print(f"Float: {round(number, 2)}")
