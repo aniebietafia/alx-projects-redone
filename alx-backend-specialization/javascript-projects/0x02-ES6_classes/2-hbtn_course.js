021c6c7bbf59117f9792dbf40cb7b63db06f7e63
aniebietafia <aniebietafia87@gmail.com>
2023-08-27 19:03:44 +0100

    getters and setters

--- alx-backend-specialization/javascript-projects/0x02-ES6_classes/2-hbtn_course.js
+++ alx-backend-specialization/javascript-projects/0x02-ES6_classes/2-hbtn_course.js
@@ -8,36 +8,30 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
-
   set name(newName) {
-    if (typeof newName === "string") {
-      this._name = newName;
-    } else {
+    if (typeof newName !== "string") {
       throw new TypeError("Name must be a string");
     }
+    this._name = newName;
   }
 
   get length() {
     return this._length;
   }
-
   set length(newLength) {
-    if (typeof newLength === "number") {
-      this._length = newLength;
-    } else {
+    if (typeof newLength !== "number") {
       throw new TypeError("Length must be a number");
     }
+    this._length = newLength;
   }
 
   get students() {
     return this._students;
   }
-
   set students(newStudents) {
-    if (Array.isArray(newStudents)) {
-      this._students = newStudents;
-    } else {
-      throw new TypeError("Students must be an array");
+    if (!Array.isArray(newStudents) && newStudents.every((student) => typeof student !== "string")) {
+      throw new TypeError("Students must be an array of strings");
     }
+    this._students = newStudents;
   }
 }
