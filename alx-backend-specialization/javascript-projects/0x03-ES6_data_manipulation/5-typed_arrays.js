d321b216d45039eaee496680b1821a7202637a83
aniebietafia <aniebietafia87@gmail.com>
2023-08-30 21:07:02 +0100

    typed array

--- alx-backend-specialization/javascript-projects/0x03-ES6_data_manipulation/5-typed_arrays.js
+++ alx-backend-specialization/javascript-projects/0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,11 +4,12 @@ Create a function named createInt8TypedArray that returns a new ArrayBuffer with
 */
 
 const createInt8TypedArray = (length, position, value) => {
-  const buff = new ArrayBuffer(length);
-  const view = new DataView(buff);
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
   if (position > length - 1) {
-    throw Error("Position outside range");
+    throw new Error("Position outside range.");
   }
+
   view.setInt8(position, value);
   return view;
 };
