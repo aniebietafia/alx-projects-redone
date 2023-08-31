249008339d24f487991530bda4716b28cc554975
aniebietafia <aniebietafia87@gmail.com>
2023-08-31 12:16:25 +0100

    set methods

--- alx-backend-specialization/javascript-projects/0x03-ES6_data_manipulation/7-has_array_values.js
+++ alx-backend-specialization/javascript-projects/0x03-ES6_data_manipulation/7-has_array_values.js
@@ -2,8 +2,8 @@
 Create a function named hasValuesFromArray that returns a boolean if all the elements in the array exist within the set. It accepts two arguments: a set (Set) and an array (Array).
 */
 
-const hasValuesFromArray = (setValues, arrValues) => {
-  return arrValues.every((value) => setValues.has(value));
+const hasValuesFromArray = (set, array) => {
+  return array.every((item) => set.has(item));
 };
 
 export default hasValuesFromArray;
