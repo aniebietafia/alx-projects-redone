0f141ca0c806b53da20084db5174311022d8a98c
aniebietafia <aniebietafia87@gmail.com>
2023-09-02 19:30:38 +0100

    setting up task instructions

---
+++ alx-backend-specialization/javascript-projects/0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+/*
+Create a directory task_1 and copy these configuration files into this folder: package.json, tsconfig.json, webpack.config.js
+•	firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
+•	fullTimeEmployee(boolean) this attribute should always be defined
+•	yearsOfExperience(number) this attribute is optional
+•	location(string) this attribute should always be defined
+•	Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
+*/
