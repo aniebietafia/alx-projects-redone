e2cccfb815c857a8f047739ce2026600c065aa7b
aniebietafia <aniebietafia87@gmail.com>
2023-09-03 19:51:18 +0100

    writing a class

--- alx-backend-specialization/javascript-projects/0x04-TypeScript/task_1/js/main.ts
+++ alx-backend-specialization/javascript-projects/0x04-TypeScript/task_1/js/main.ts
@@ -49,3 +49,46 @@ export const printTeacher2: printTeacherFunction = (firstName: string, lastName:
   const firstLetterOfFirstName = firstName.slice(0, 1).toUpperCase();
   return `${firstLetterOfFirstName}. ${lastName}`;
 };
+
+// Task 1.3 - Writing a class
+/*
+Write a Class named StudentClass:
+•	The constructor accepts firstName(string) and lastName(string) arguments
+•	The class has a method named workOnHomework that return the string Currently working
+•	The class has a method named displayName. It returns the firstName of the student
+•	The constructor of the class should be described through an Interface
+•	The class should be described through an Interface
+Requirements:
+•	You can reuse the Webpack configuration from the previous exercise to compile the code.
+•	When running npm run build, no TypeScript error should be displayed.
+•	Every variable should use TypeScript when possible.
+*/
+
+export interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+};
