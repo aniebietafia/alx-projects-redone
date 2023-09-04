aa0cc2b224e758483c5c3063c32025c09e657be8
aniebietafia <aniebietafia87@gmail.com>
2023-09-04 21:06:14 +0100

    subjects interface

---
+++ alx-backend-specialization/javascript-projects/0x04-TypeScript/task_4 /js/subjects/Cpp.ts
@@ -0,0 +1,23 @@
+//  Create a file Cpp.ts and make the following modifications in the same namespace. Using declaration merging, add a new optional attribute experienceTeachingC (number) to the Teacher interface
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+}
+
+//  Create a class Cpp extending from Subject
+export default class Cpp extends Subject {
+  //  Write a method named getRequirements that will return a string Here is the list of requirements for Cpp
+  getRequirements(): string {
+    return "Here is the list of requirements for Cpp";
+  }
+  //  Write a method named getAvailableTeacher that will return a string Available Teacher: <first name of teacher>
+  getAvailableTeacher(): string {
+    //  If the teacher doesn’t have any experience in teaching C, then the method should return a string No available teacher
+    if (!this.teacher.experienceTeachingC) {
+      return "No available teacher";
+    }
+    return `Available Teacher: ${this.teacher.firstName}`;
+  }
+}
