a5f557d8ef081f9669f6fb9d6102f3654bff7fc0
aniebietafia <aniebietafia87@gmail.com>
2023-10-17 20:04:44 +0100

    test case: error test case

---
+++ alx-backend-specialization/javascript-projects/0x05-Node_JS_basic/3-main_0.js
@@ -0,0 +1,9 @@
+import countStudents from "./3-read_file_async.js";
+
+countStudents("nope.csv")
+  .then(() => {
+    console.log("Done!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
