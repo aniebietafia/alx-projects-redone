8dbc8e3f0b1b29d9d742e7098934d66091a70e43
aniebietafia <aniebietafia87@gmail.com>
2024-02-22 17:33:43 +0100

    implementing class with getters & setters

--- alx-backend-javascript/0x02-ES6_classes/2-hbtn_course.js
+++ alx-backend-javascript/0x02-ES6_classes/2-hbtn_course.js
@@ -1,2 +1,19 @@
 export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students;
+
+        if (typeof this._name !== "string"){
+            throw new Error(`${this._name} should be a string`);
+        }
+
+        if (typeof Number(this._length) !== "number") {
+            throw new Error(`${this._length} should be a number`);
+        }
+
+        if (!Array.isArray(students)) {
+            throw new Error(`${this._students} is not an array.`);
+        }
+    }
 }
