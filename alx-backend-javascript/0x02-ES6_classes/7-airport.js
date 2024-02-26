923bac0a26099f26557ee6a65dae4ca8ee22075e
aniebietafia <aniebietafia87@gmail.com>
2024-02-26 11:35:46 +0100

    implementing airport class

---
+++ alx-backend-javascript/0x02-ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+export default class Airport {
+    constructor(name, code) {
+        this._name = name;
+        this._code = code;
+    }
+
+    get [Symbol.toStringTag]() {
+        return `${this._code}`;
+    }
+
+
+}
