7fd23f4b6b78549cddc0e24e1e48cb0bdc8bc815
aniebietafia <aniebietafia87@gmail.com>
2024-02-26 10:53:02 +0100

    implementing pricing

--- alx-backend-javascript/0x02-ES6_classes/4-pricing.js
+++ alx-backend-javascript/0x02-ES6_classes/4-pricing.js
@@ -23,7 +23,7 @@ export default class Pricing {
     }
 
     displayFullPrice() {
-        return `${this._amount} ${this._currency}`
+        return `${this._amount} ${this._currency._code} (${this._currency._name})`;
     }
 
     static convertPrice(amount, conversionRate) {
