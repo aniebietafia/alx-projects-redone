e39e9020d9e19ebe57df06d640425f1da2fa3d75
aniebietafia <aniebietafia87@gmail.com>
2024-02-28 12:28:26 +0100

    Set data structure

---
+++ alx-backend-javascript/0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,3 @@
+export default function setFromArray(element) {
+    return new Set(element);
+}
---
+++ alx-backend-javascript/0x03-ES6_data_manipulation/test-files/6-main.js
@@ -0,0 +1,5 @@
+import setFromArray from "../6-set.js";
+
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
+console.log(setFromArray([3, 8, 3, 5, 9, 2, 8, 10]));
+
