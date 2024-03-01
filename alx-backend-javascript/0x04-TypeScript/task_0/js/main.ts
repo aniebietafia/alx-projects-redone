dc66eeb9db7aa339aa308077998acc92fa459e27
aniebietafia <aniebietafia87@gmail.com>
2024-03-01 12:58:08 +0100

    added student interface

--- alx-backend-javascript/0x04-TypeScript/task_0/js/main.ts
+++ alx-backend-javascript/0x04-TypeScript/task_0/js/main.ts
@@ -19,15 +19,36 @@ const student_2: Student = {
     location: 'Star City',
 }
 
-const studentsList = [student_1, student_2];
+const studentsList: Student[] = [student_1, student_2];
 
 const createStudentTable = (studentObject) => {
     const table = document.createElement('table');
-    const th = document.createElement('th');
-    const tr = document.createElement('tr');
-    const td = document.createElement('td');
+
+    const tableHeader1 = document.createElement('th');
+    const tableHeader2 = document.createElement('th');
+
+    const tableRow1 = document.createElement('tr');
+    const tableRow2 = document.createElement('tr');
+
+    const tableData1 = document.createElement('td');
+    const tableData2 = document.createElement('td');
+
+    document.body.appendChild(table);
+
+    tableHeader1.textContent = "First Name";
+    tableHeader2.textContent = "Location";
+
+    tableRow1.appendChild(tableHeader1);
+    tableRow1.appendChild(tableHeader2);
+
+    tableRow2.appendChild(tableData1);
+    tableRow2.appendChild(tableData2);
 
     for (const student in studentObject) {
-
+        tableData1.textContent = student.firstName;
+        tableData2.textContent = student.location;
     }
+
+    table.appendChild(tableRow1);
+    table.appendChild(tableRow2);
 }
