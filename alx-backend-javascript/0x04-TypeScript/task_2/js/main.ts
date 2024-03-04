436d4769b09d1fa49d613b6659ebaf71c3bdba6a
aniebietafia <aniebietafia87@gmail.com>
2024-03-04 13:33:34 +0100

    added execute tasks function

--- alx-backend-javascript/0x04-TypeScript/task_2/js/main.ts
+++ alx-backend-javascript/0x04-TypeScript/task_2/js/main.ts
@@ -38,7 +38,7 @@ class Teacher implements TeacherInterface {
 	}
 }
 
-export default function createEmployee(salary: string | number): Teacher | Director {
+export function createEmployee(salary: string | number): Teacher | Director {
 	if (typeof salary === "number" && salary < 500) {
 		const teacher = new Teacher();
 		return teacher;
@@ -54,7 +54,7 @@ const isDirector = (employee) => {
 	return director;
 }
 
-const executeWork = (employee) => {
+export const executeWork = (employee) => {
 	if (employee === "Director") {
 		const directorTasks = isDirector(employee).workDirectorTasks();
 		return directorTasks;
