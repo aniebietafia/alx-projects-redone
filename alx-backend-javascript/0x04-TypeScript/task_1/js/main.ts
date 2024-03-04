ce3ae8d1ff981b822435525ad0ba882ca2eb2b08
aniebietafia <aniebietafia87@gmail.com>
2024-03-04 11:57:44 +0100

    added directors interface that extends from teacher interface

--- alx-backend-javascript/0x04-TypeScript/task_1/js/main.ts
+++ alx-backend-javascript/0x04-TypeScript/task_1/js/main.ts
@@ -20,3 +20,12 @@ console.log(teacher3);
 interface Directors extends Teacher {
 	numberOfReports: number;
 }
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
