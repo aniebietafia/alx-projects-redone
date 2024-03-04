b9cf770ecefc21bbfc0a2d309b55d42c38ef2c59
aniebietafia <aniebietafia87@gmail.com>
2024-03-04 12:06:50 +0100

    added print teacher function with the interface

--- alx-backend-javascript/0x04-TypeScript/task_1/js/main.ts
+++ alx-backend-javascript/0x04-TypeScript/task_1/js/main.ts
@@ -29,3 +29,11 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+interface printTeacherFunction {
+	printTeacher: (firstName: string, lastName: string) => string;
+}
+
+function printTeacher: printTeacherFunction(firstName: string, lastName: string): string {
+	return `${firstName.charAt(0)}. ${lastName}`
+}
