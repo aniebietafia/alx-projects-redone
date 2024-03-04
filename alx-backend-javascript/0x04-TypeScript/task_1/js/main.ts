36b23e07954f5d5217b6f74217326342537e031c
aniebietafia <aniebietafia87@gmail.com>
2024-03-04 11:51:02 +0100

    Defined Teacher interface

--- alx-backend-javascript/0x04-TypeScript/task_1/js/main.ts
+++ alx-backend-javascript/0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,13 @@ interface Teacher {
 	location: string;
 	[key: string]: boolean;
 }
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
