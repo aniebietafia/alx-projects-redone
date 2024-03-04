9439ee38e2358400bdc2736ff01d6e50907074c6
aniebietafia <aniebietafia87@gmail.com>
2024-03-04 13:33:38 +0100

    test file for main

--- alx-backend-javascript/0x04-TypeScript/task_2/js/main_test.ts
+++ alx-backend-javascript/0x04-TypeScript/task_2/js/main_test.ts
@@ -1,5 +1,9 @@
-import createEmployee from "./main";
+import { createEmployee, executeWork } from "./main";
 
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
