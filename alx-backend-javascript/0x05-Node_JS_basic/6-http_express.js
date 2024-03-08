2492543c2b4d33fccfaa7f3b4e1a528b0f148714
aniebietafia <aniebietafia87@gmail.com>
2024-03-08 16:37:25 +0100

    simple server using express

--- alx-backend-javascript/0x05-Node_JS_basic/6-http_express.js
+++ alx-backend-javascript/0x05-Node_JS_basic/6-http_express.js
@@ -1,17 +1,14 @@
-import http from "node:http";
 import express from "express";
 
 const app = express();
 
 const PORT = 1245;
 
-app.get("/", () => {
-	console.log('Hello Holberton School!');
-});
+app.get("/", (req, res) => {
+	res.send('Hello Holberton School!');
+})
 
-const server = http.createServer(app);
-
-server.listen(PORT, function() {
+app.listen(PORT, function() {
 	console.log("Server listening on port: " + PORT);
 });
 
