57f08cc46adbfe7a3f8c58c33af6828ec4814890
aniebietafia <aniebietafia87@gmail.com>
2024-03-12 17:42:45 +0100

    added try catch block

--- alx-backend-javascript/0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ alx-backend-javascript/0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,10 +3,10 @@ import readDatabase from "../utils.js";
 export class StudentsController {
 	static async getAllStudents(req, res) {
 		try {
-			const students = await readDatabase("./database.csv");
+			// const students = await readDatabase("../database.csv");
 			this.res.writeHead(200)
 			this.res.write("This is the list of our students");
-			this.res
+			// console.log(students);
 		} catch (e) {
 			throw new Error("Cannot load the database.");
 		}
