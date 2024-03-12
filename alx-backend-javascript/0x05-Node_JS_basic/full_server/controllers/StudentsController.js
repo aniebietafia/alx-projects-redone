5b95504932da0c8b026d72e4d07ab5ccbba85d0d
aniebietafia <aniebietafia87@gmail.com>
2024-03-12 18:20:30 +0100

    student class

--- alx-backend-javascript/0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ alx-backend-javascript/0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,11 +3,17 @@ import readDatabase from "../utils.js";
 export class StudentsController {
 	static async getAllStudents(req, res) {
 		try {
-			const students = await readDatabase("../../database.csv");
-			res.send("This is the list of our students");
-			console.log(students);
+			const students = await readDatabase("./database.csv");
+			const result = [];
+			
+			result.push("This is the list of our students");
+
+			for (const key in students) {
+				result.push(`Number of students in ${key}: ${students[key].length}. List: ${students[key].join(", ")}`)
+			}
+			res.send(result.join("\n"));
 		} catch (e) {
-			throw new Error("Cannot load the database.");
+			res.send(500, "Cannot load the database.");
 		}
 	}
 }
