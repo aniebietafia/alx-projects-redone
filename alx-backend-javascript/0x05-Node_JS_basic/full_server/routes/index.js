a1d4fbcc32b4fc5393bac5c63d994d7aa7e07acf
aniebietafia <aniebietafia87@gmail.com>
2024-03-13 08:46:46 +0100

    added routes

--- alx-backend-javascript/0x05-Node_JS_basic/full_server/routes/index.js
+++ alx-backend-javascript/0x05-Node_JS_basic/full_server/routes/index.js
@@ -4,3 +4,9 @@ const router = express.Router();
 
 import StudentsController from "../controllers/StudentsController.js";
 import AppController from "../controllers/AppController.js";
+
+router.get("/", AppController.getHomepage);
+router.get("/students", StudentsController.getAllStudents);
+router.get("/students/:major", StudentsController.getAllStudentsByMajor);
+
+export default router;
