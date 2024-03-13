609677ec5e978d9cb3e4ac4184f3f83156118f4e
aniebietafia <aniebietafia87@gmail.com>
2024-03-13 08:29:23 +0100

    added get all students by major

--- alx-backend-javascript/0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ alx-backend-javascript/0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,15 +5,44 @@ export class StudentsController {
 		try {
 			const students = await readDatabase("./database.csv");
 			const result = [];
-			
+
 			result.push("This is the list of our students");
 
 			for (const key in students) {
 				result.push(`Number of students in ${key}: ${students[key].length}. List: ${students[key].join(", ")}`)
 			}
-			res.send(result.join("\n"));
+			res.send(200, result.join("\n"));
 		} catch (e) {
 			res.send(500, "Cannot load the database.");
+		}
+	}
+
+	static async getAllStudentsByMajor(req, res) {
+		try {
+			const { major } = req.params;
+
+			if (major !== "CS" || major !== "SWE") {
+				throw new Error("Major parameter must be CS or SWE");
+			}
+
+			const students = await readDatabase('./database.csv');
+
+			if (!students) {
+				throw new Error("Cannot load the database");
+			}
+
+			const result = [];
+
+			for (const key in students) {
+				if (major === key) {
+					result.push(`List: ${students[key]}`)
+				}
+			}
+
+			res.send(result.join(', '));
+			
+		} catch (e) {
+			res.send(500, e.message);
 		}
 	}
 }
