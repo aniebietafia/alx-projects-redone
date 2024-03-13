a4e454b572520d87f8484181a5f3b8250f716656
aniebietafia <aniebietafia87@gmail.com>
2024-03-13 08:37:20 +0100

    added the imports

---
+++ alx-backend-javascript/0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+const router = express.Router();
+
+import StudentsController from "../controllers/StudentsController.js";
+import AppController from "../controllers/AppController.js";
